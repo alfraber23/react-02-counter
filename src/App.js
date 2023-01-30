d0908df3718a02c04e2d263dba3e35c72c88180c
Beto <josealberto.fb15@gmail.com>
2023-01-30 13:45:58 -0600

    Ci: routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import { Counter } from './components/Counter-UseState';
-import { Routes, Route, NavLink } from 'react-router-dom'
+import { Routes, Route, NavLink, Navigate } from 'react-router-dom'
 import { Home } from './components/Home/Home'
 import { Login } from './components/Login/Login';
+import { Error404 } from './components/Error404/Error404';
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/counter-app" element={<Counter initialValue={0}/>} />
           <Route path="/login" element={ <Login /> } />
+          <Route path="/404" element={ <Error404 /> } />
+          <Route path="/*" element={ <Navigate to="/404" /> } />
           
         </Routes>
         
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+const users = [
+    {
+        idUser: 123456,
+        name: "Alberto",
+        cohorte: 22,
+    },
+    {
+        idUser: 123,
+        name: "Juan",
+        cohorte: 22,
+    }
+]
+
 export const Home = () => {
+
+    const listItems = users.map(user => <li key={user.idUser}>{user.name} de la cohorte {user.cohorte}</li>);
+
     return (
         <div>
             <h1>Home</h1>
+            <ul>
+                {listItems}
+            </ul>
         </div>
     )
 }
